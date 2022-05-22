e5078a8828b8c41ed2bbd2d65fb24ac3d988eac0
Angelos <v.pazaitis@web.de>
2022-05-22 11:45:00 +0200

    design update gui

---
+++ .output/server/chunks/app/client.manifest.mjs
@@ -0,0 +1,13 @@
+const client_manifest = {
+  "node_modules/nuxt/dist/app/entry.mjs": {
+    "file": "entry-a694d750.mjs",
+    "src": "node_modules/nuxt/dist/app/entry.mjs",
+    "isEntry": true,
+    "css": [
+      "entry.d4d2bd9d.css"
+    ]
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
+
 <template>
-  <div class="navbar bg-base-100">
+<html data-theme="cupcake"></html>
+
+  <div class="navbar bg-base-accent">
     <div class="flex-1">
       <a class="btn btn-ghost normal-case text-xl">Spellphabet</a>
     </div>
@@ -31,36 +34,52 @@
     </div>
   </div>
 
-  <div class="bg-base-100">
+  <div class="bg-base-accent">
     <div class="hero-content text-center">
       <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Hello there</h1>
-        <p class="py-6">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
-          id nisi.
-        </p>
+        <h1 class="text-5xl font-bold">Just type and get it!</h1>
+       
       </div>
+  </div>
     </div>
-  </div>
+<div class=" justify-center text-center ">
+   <p class="py-6">
+        Or just try examples
+    </p>
+    <button class="btn gap-2">
+  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" /></svg>
+  Refresh
+</button>
 
-  <div class="flex justify-center">
-    <input
-      type="text"
-      placeholder="Type here"
-      class="input input-bordered w-full max-w-xs justify-center"
-    />
+
+<a role="button" class="btn bg-accent">Link</a>
+<button type="submit" class="btn bg-secondary-focus">Button</button>
+<input type="button" value="Input" class="btn" />
+<input type="submit" value="Submit" class="btn" />
+<input type="reset" value="Reset" class="btn" />
+
+</div>
+
+<div class="form-control flex justify-center">
+  <div class="input-group flex justify-center">
+    <input type="text" placeholder="Type here" class="input input-bordered" />
+    <button class="btn btn-square">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+    </button>
   </div>
-  <div class="flex justify-center">
+</div>
+
+<div class="flex justify-center">
     <div class="form-control">
       <label class="label cursor-pointer">
         <span class="label-text">Auto-Language</span>
         <input
           type="checkbox"
-          class="toggle toggle-sm toggle-secondary"
+          class="toggle toggle-sm toggle-accent"
           checked
         />
       </label>
     </div>
-  </div>
+</div>
+
 </template>
